09a5feac013fd190bb1f55fdac1c86ebd3329d1e
nyeinchanaungnca <kumarkalar28@gmail.com>
2025-09-07 15:41:47 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# === MANDATORY VARIABLES (Fill all of these with your information) ===
+
+# Your bot's token from @BotFather on Telegram.
+BOT_TOKEN = "8159632223:AAGXRDbxg7R2wDxgExM4pX-nNjvstr7WeD0"
+
+# Your personal Telegram User ID (must be a number).
+OWNER_ID = 7371701519  # PASTE YOUR TELEGRAM ID HERE
+
+# Get these two values from my.telegram.org.
+TELEGRAM_API = 27863773 # PASTE YOUR API ID HERE (it's a number)
+TELEGRAM_HASH = "9f5cee92c1878b2d9c10c84a5eedbf2e"
+
+# A strong, random password that you create to encrypt the bot's database.
+# Example: "MyS3cur3P@ssw0rd!_For-K0yeb"
+DATABASE_ENCRYPTION_KEY = "Aung45&2"
+
+# Your MongoDB connection URL.
+DATABASE_URL = "mongodb+srv://nyeinchanaung:aung12345@cluster0.q5labmg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
